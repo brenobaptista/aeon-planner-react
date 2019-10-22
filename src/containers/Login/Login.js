5625eea144cc5c88620690d0dc3a4df4e59efa26
brenobaptista <brenomb07@hotmail.com>
2019-10-22 18:51:58 -0300

    Fix Login Bug

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import classes from './Login.module.css';
 import FormError from '../../components/FormError/FormError';
-import axios from 'axios';
+import Spinner from '../../components/Spinner/Spinner';
 
 class Login extends Component {
   state = {
     email: '',
     password: '',
-    error: false
+    error: false,
+    authLoading: false,
   }
 
   loginHandler = async () => {
+    this.setState({ authLoading: true })
     const data = {
       email: this.state.email,
-      password: this.state.email
+      password: this.state.password
     }
     try {
-      await axios.post('https://trello-api-nodejs.herokuapp.com/login', data)
+      const res = await axios.post('https://trello-api-nodejs.herokuapp.com/login', data)
+      this.setState({ authLoading: false })
+      console.log(res.data.token)
+      console.log(res.data.userId)
     } catch {
-      this.setState({ error: true })
+      this.setState({ 
+        error: true,
+        authLoading: false
+      })
     }
   }
 
@@ -30,18 +39,20 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <center>
-          <h1 className="margin-t-b">Login</h1><br />
-          <label>Email:</label><br />
-          <input type="email" placeholder="Email" value={this.state.email} onChange={this.emailHandler} /><br /><br />
-          <label>Password:</label><br />
-          <input type="password" placeholder="Password" value={this.state.password} onChange={this.passwordHandler} /><br /><br />
-          <button className="btn btn-danger margin-teeth" onClick={this.props.history.goBack}>Cancel</button>
+        {this.state.authLoading ? <Spinner /> :
+          <center>
+            <h1 className="margin-t-b">Login</h1><br />
+            <label>Email:</label><br />
+            <input type="email" placeholder="Email" value={this.state.email} onChange={this.emailHandler} /><br /><br />
+            <label>Password:</label><br />
+            <input type="password" placeholder="Password" value={this.state.password} onChange={this.passwordHandler} /><br /><br />
+            <button className="btn btn-danger margin-teeth" onClick={this.props.history.goBack}>Cancel</button>
 
-          <button className="btn btn-success margin-teeth" onClick={this.loginHandler}>Login</button><br /><br />
-          <Link to="/signup"><b className={classes.greenLink}>I don't have an account yet</b></Link>
-          {this.state.error ? <FormError /> : null}
-        </center>
+            <button className="btn btn-success margin-teeth" onClick={this.loginHandler}>Login</button><br /><br />
+            <Link to="/signup"><b className={classes.greenLink}>I don't have an account yet</b></Link>
+            {this.state.error ? <FormError /> : null}
+          </center>
+        }
       </div>
     )
   }
--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import FormError from '../../components/FormError/FormError';
 import axios from 'axios';
+
+import FormError from '../../components/FormError/FormError';
+import Spinner from '../../components/Spinner/Spinner';
 
 class SignUp extends Component {
   state = {
@@ -8,22 +10,29 @@ class SignUp extends Component {
     email: '',
     password: '',
     passwordAgain: '',
-    error: false
+    error: false,
+    authLoading: false,
   }
 
   signUpHandler = async () => {
     try {
       if (this.state.password === this.state.passwordAgain) {
+        this.setState({ authLoading: true })
         const data = {
           name: this.state.name,
           email: this.state.email,
-          password: this.state.email
+          password: this.state.password
         }
-      await axios.put('https://trello-api-nodejs.herokuapp.com/signup', data)
-      this.props.history.push('login');
+        const res = await axios.put('https://trello-api-nodejs.herokuapp.com/signup', data)
+        console.log(res)
+        this.setState({ authLoading: false })
+        this.props.history.push('login');
       }
     } catch {
-      this.setState({ error: true })
+      this.setState({ 
+        error: true,
+        authLoading: false
+      })
     }
   }
 
@@ -35,21 +44,23 @@ class SignUp extends Component {
   render() {
     return (
       <div>
-        <center>
-          <h1 className="margin-t-b">Sign Up</h1><br />
-          <label>Name:</label><br />
-          <input type="text" placeholder="Name" value={this.state.name} onChange={this.nameHandler} /><br /><br />
-          <label>Email:</label><br />
-          <input type="email" placeholder="Email" value={this.state.email} onChange={this.emailHandler} /><br /><br />
-          <label>Password:</label><br />
-          <input type="password" placeholder="Password" value={this.state.password} onChange={this.passwordHandler} /><br /><br />
-          <label>Password again:</label><br />
-          <input type="password" placeholder="Password" value={this.state.passwordAgain} onChange={this.passwordAgainHandler} /><br /><br />
-          <button className="btn btn-danger margin-teeth" onClick={this.props.history.goBack}>Cancel</button>
+        {this.state.authLoading ? <Spinner /> :
+          <center>
+            <h1 className="margin-t-b">Sign Up</h1><br />
+            <label>Name:</label><br />
+            <input type="text" placeholder="Name" value={this.state.name} onChange={this.nameHandler} /><br /><br />
+            <label>Email:</label><br />
+            <input type="email" placeholder="Email" value={this.state.email} onChange={this.emailHandler} /><br /><br />
+            <label>Password:</label><br />
+            <input type="password" placeholder="Password" value={this.state.password} onChange={this.passwordHandler} /><br /><br />
+            <label>Password again:</label><br />
+            <input type="password" placeholder="Password" value={this.state.passwordAgain} onChange={this.passwordAgainHandler} /><br /><br />
+            <button className="btn btn-danger margin-teeth" onClick={this.props.history.goBack}>Cancel</button>
 
-          <button className="btn btn-success margin-teeth" onClick={this.signUpHandler}>Sign Up</button>
-          {this.state.error ? <FormError /> : null}
-        </center>
+            <button className="btn btn-success margin-teeth" onClick={this.signUpHandler}>Sign Up</button>
+            {this.state.error ? <FormError /> : null}
+          </center>
+        }
       </div>
     )
   }
